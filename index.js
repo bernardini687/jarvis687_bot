4b95d6bc9d11331f6bf01749fcf36cc7e68b4818
oscar bernardini <bernardini687@gmail.com>
2021-06-30 16:52:32 +0200

    update

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const axios = require('axios')
+const axios = require('axios');
+
 const BASE_URL = `https://api.telegram.org/bot${process.env['BOT_TOKEN']}`;
 
-exports.handler = async event => {
-  console.log("[INFO] event:", event);
+exports.handler = async (event) => {
+  const msg = telegramMessage(event);
 
-  const message = telegramMessage(event);
+  try {
+    const { data } = await sendMessage(msg.chat.id, msg.text);
+    console.log("data:", data);
 
-  console.log("[INFO] text:", message.text);
+    return { statusCode: 200 };
+  } catch (err) {
+    console.log("err:", err);
 
-  sendMessage(message.chat.id, message.text);
-
-  return {
-    statusCode: 200
-  };
+    return { statusCode: 500 };
+  }
 };
 
 /*
@@ -29,22 +31,13 @@ message: {
 }
  */
 function telegramMessage(event) {
-  const { message } = JSON.parse(event.body);
-
-  return message;
+  return JSON.parse(event.body).message;
 }
 
 function sendMessage(chat_id, text) {
-  axios
-    .post(`${BASE_URL}/sendMessage`, {
-      chat_id,
-      text,
-      parse_mode: 'HTML'
-    })
-    .then(res => {
-      console.log(`[INFO] res.statusCode: ${res.statusCode}`);
-    })
-    .catch(err => {
-      console.error(err);
-    });
+  return axios.post(`${BASE_URL}/sendMessage`, {
+    chat_id,
+    text,
+    parse_mode: 'HTML'
+  });
 }
