5247943d7906d4a4753a297ce953bfbb25292c94
oscar bernardini <bernardini687@gmail.com>
2021-07-05 08:52:51 +0200

    test bad input

--- src/index.test.js
+++ src/index.test.js
@@ -17,26 +17,28 @@ describe('scenario', () => {
   const SEND_MESSAGE_URL = 'https://api.telegram.org/bot12345/sendMessage'
   const MEMORY_KEY = 'balance/memory.json'
 
-  describe('unpermitted user sends a message', () => {
-    it('sends an error message back', async () => {
-      const event = require('../test/events/unpermitted_user')
+  describe('unpermitted user', () => {
+    describe ('sends a message', () => {
+      it('sends an error message back', async () => {
+        const event = require('../test/events/unpermitted_user')
 
-      await index.handler(event)
+        await index.handler(event)
 
-      expect(bucketMock.readJsonContent).not.toHaveBeenCalled()
-      expect(bucketMock.writeJsonContent).not.toHaveBeenCalled()
-      expect(axiosMock.post).toHaveBeenCalledTimes(1)
-      expect(axiosMock.post).toHaveBeenCalledWith(
-        SEND_MESSAGE_URL,
-        {
-          chat_id: 9,
-          text: texts.UNPERMITTED_USER
-        }
-      )
+        expect(bucketMock.readJsonContent).not.toHaveBeenCalled()
+        expect(bucketMock.writeJsonContent).not.toHaveBeenCalled()
+        expect(axiosMock.post).toHaveBeenCalledTimes(1)
+        expect(axiosMock.post).toHaveBeenCalledWith(
+          SEND_MESSAGE_URL,
+          {
+            chat_id: 9,
+            text: texts.UNPERMITTED_USER
+          }
+        )
+      })
     })
   })
 
-  describe('permitted user inputs 2 initial expenses', () => {
+  describe('permitted user', () => {
     beforeEach(() => {
       // in this scenario, `RESETBALANCE` was first invoked, causing the memory to become initialized
       bucketMock.readJsonContent.mockImplementation(() => Promise.resolve({ users: {}, history: {} }))
@@ -52,102 +54,158 @@ describe('scenario', () => {
       selective: true
     }
 
-    it('updates the memory accordingly', async () => {
-      /*
-       * INTERACTION #1
-       *
-       * First user (id: 111) sends `/spesa` command:
-       */
-      let event = require('../test/events/user_1_spesa_1')
-      let expectedMemory = {
-        users: { 111: { name: 'user_1', sign: '+' } },
-        history: {}
-      }
-
-      await index.handler(event)
-
-      expect(bucketMock.readJsonContent).toHaveBeenCalledTimes(1) // get the state of memory
-      expect(bucketMock.writeJsonContent).toHaveBeenCalledTimes(1) // update with the new user
-      expect(bucketMock.writeJsonContent).toHaveBeenCalledWith(MEMORY_KEY, expectedMemory)
-      expect(axiosMock.post).toHaveBeenCalledTimes(1) // send expense request
-      expect(axiosMock.post).toHaveBeenCalledWith(
-        SEND_MESSAGE_URL,
-        {
-          chat_id: 1,
-          text: texts.BALANCE_QUERY('user_1'),
-          reply_to_message_id: 62,
-          reply_markup
+    describe('inputs something other than a number', () => {
+      it('sends an error message back', async () => {
+        /*
+         * INTERACTION #1
+         *
+         * First user (id: 111) sends `/spesa` command:
+         */
+        let event = require('../test/events/user_1_spesa_1')
+        let expectedMemory = {
+          users: { 111: { name: 'user_1', sign: '+' } },
+          history: {}
         }
-      )
 
-      /*
-       * INTERACTION #2
-       *
-       * User 111 responds to the bot's expense request with `12.01`:
-       */
-      event = require('../test/events/user_1_spesa_2')
-      updateMemory(expectedMemory) // respond to reads with the current memory state
-      expectedMemory = { ...expectedMemory, history: { user_1: 1201 } }
+        await index.handler(event)
 
-      await index.handler(event)
+        expect(bucketMock.readJsonContent).toHaveBeenCalledTimes(1) // get the state of memory
+        expect(bucketMock.writeJsonContent).toHaveBeenCalledTimes(1) // update with the new user
+        expect(bucketMock.writeJsonContent).toHaveBeenCalledWith(MEMORY_KEY, expectedMemory)
+        expect(axiosMock.post).toHaveBeenCalledTimes(1) // send expense request
+        expect(axiosMock.post).toHaveBeenCalledWith(
+          SEND_MESSAGE_URL,
+          {
+            chat_id: 1,
+            text: texts.BALANCE_QUERY('user_1'),
+            reply_to_message_id: 62,
+            reply_markup
+          }
+        )
 
-      expect(bucketMock.readJsonContent).toHaveBeenCalledTimes(2)
-      expect(bucketMock.writeJsonContent).toHaveBeenCalledTimes(2)
-      expect(bucketMock.writeJsonContent).toHaveBeenCalledWith(MEMORY_KEY, expectedMemory)
-      expect(axiosMock.post).toHaveBeenCalledTimes(2)
-      expect(axiosMock.post).toHaveBeenCalledWith(
-        SEND_MESSAGE_URL,
-        {
-          chat_id: 1,
-          text: 'user_1: 12.01'
+        /*
+         * INTERACTION #2
+         *
+         * User 111 responds to the bot's expense request with `hello`:
+         */
+        event = require('../test/events/user_1_bad_input')
+        updateMemory(expectedMemory) // respond to reads with the current memory state
+
+        await index.handler(event)
+
+        expect(bucketMock.readJsonContent).toHaveBeenCalledTimes(2)
+        expect(bucketMock.writeJsonContent).toHaveBeenCalledTimes(1) // update is skipped because of bad input
+        expect(axiosMock.post).toHaveBeenCalledTimes(2)
+        expect(axiosMock.post).toHaveBeenCalledWith(
+          SEND_MESSAGE_URL,
+          {
+            chat_id: 1,
+            text: texts.BALANCE_INPUT_ERROR,
+            reply_to_message_id: 64,
+            reply_markup
+          }
+        )
+      })
+    })
+
+    describe('inputs 2 initial expenses', () => {
+      it('updates the memory accordingly', async () => {
+        /*
+         * INTERACTION #1
+         *
+         * First user (id: 111) sends `/spesa` command:
+         */
+        let event = require('../test/events/user_1_spesa_1')
+        let expectedMemory = {
+          users: { 111: { name: 'user_1', sign: '+' } },
+          history: {}
         }
-      )
 
-      // /*
-      //  * INTERACTION #3
-      //  *
-      //  * User 111 sends `/spesa` command again:
-      //  */
-      // event = require('../test/events/user_1_spesa_3')
-      // updateMemory(expectedMemory) // respond to reads with the current memory state.
+        await index.handler(event)
 
-      // await index.handler(event)
+        expect(bucketMock.readJsonContent).toHaveBeenCalledTimes(1) // get the state of memory
+        expect(bucketMock.writeJsonContent).toHaveBeenCalledTimes(1) // update with the new user
+        expect(bucketMock.writeJsonContent).toHaveBeenCalledWith(MEMORY_KEY, expectedMemory)
+        expect(axiosMock.post).toHaveBeenCalledTimes(1) // send expense request
+        expect(axiosMock.post).toHaveBeenCalledWith(
+          SEND_MESSAGE_URL,
+          {
+            chat_id: 1,
+            text: texts.BALANCE_QUERY('user_1'),
+            reply_to_message_id: 62,
+            reply_markup
+          }
+        )
 
-      // expect(bucketMock.readJsonContent).toHaveBeenCalledTimes(3)
-      // expect(bucketMock.writeJsonContent).toHaveBeenCalledTimes(2) // user 111 is already in memory, no need for any writes this time
-      // expect(axiosMock.post).toHaveBeenCalledTimes(3)
-      // expect(axiosMock.post).toHaveBeenCalledWith(
-      //   SEND_MESSAGE_URL,
-      //   {
-      //     chat_id: 1,
-      //     text: 'Quanto hai speso, user_1?',
-      //     reply_to_message_id: 3,
-      //     reply_markup
-      //   }
-      // )
+        /*
+         * INTERACTION #2
+         *
+         * User 111 responds to the bot's expense request with `12.01`:
+         */
+        event = require('../test/events/user_1_spesa_2')
+        updateMemory(expectedMemory) // respond to reads with the current memory state
+        expectedMemory = { ...expectedMemory, history: { user_1: 1201 } }
 
-      // /*
-      //  * INTERACTION #4
-      //  *
-      //  * User 111 responds to the bot with another `12.01`:
-      //  */
-      // event = require('../test/events/user_1_spesa_4')
-      // updateMemory(expectedMemory)
-      // expectedMemory = { ...expectedMemory, history: { user_1: 2402 } }
+        await index.handler(event)
 
-      // await index.handler(event)
+        expect(bucketMock.readJsonContent).toHaveBeenCalledTimes(2)
+        expect(bucketMock.writeJsonContent).toHaveBeenCalledTimes(2)
+        expect(bucketMock.writeJsonContent).toHaveBeenCalledWith(MEMORY_KEY, expectedMemory)
+        expect(axiosMock.post).toHaveBeenCalledTimes(2)
+        expect(axiosMock.post).toHaveBeenCalledWith(
+          SEND_MESSAGE_URL,
+          {
+            chat_id: 1,
+            text: 'user_1: 12.01'
+          }
+        )
 
-      // expect(bucketMock.readJsonContent).toHaveBeenCalledTimes(4)
-      // expect(bucketMock.writeJsonContent).toHaveBeenCalledTimes(3)
-      // expect(bucketMock.writeJsonContent).toHaveBeenCalledWith(MEMORY_KEY, expectedMemory)
-      // expect(axiosMock.post).toHaveBeenCalledTimes(4)
-      // expect(axiosMock.post).toHaveBeenCalledWith(
-      //   SEND_MESSAGE_URL,
-      //   {
-      //     chat_id: 1,
-      //     text: 'user_1: 24.02'
-      //   }
-      // )
+        // /*
+        //  * INTERACTION #3
+        //  *
+        //  * User 111 sends `/spesa` command again:
+        //  */
+        // event = require('../test/events/user_1_spesa_3')
+        // updateMemory(expectedMemory) // respond to reads with the current memory state.
+
+        // await index.handler(event)
+
+        // expect(bucketMock.readJsonContent).toHaveBeenCalledTimes(3)
+        // expect(bucketMock.writeJsonContent).toHaveBeenCalledTimes(2) // user 111 is already in memory, no need for any writes this time
+        // expect(axiosMock.post).toHaveBeenCalledTimes(3)
+        // expect(axiosMock.post).toHaveBeenCalledWith(
+        //   SEND_MESSAGE_URL,
+        //   {
+        //     chat_id: 1,
+        //     text: 'Quanto hai speso, user_1?',
+        //     reply_to_message_id: 3,
+        //     reply_markup
+        //   }
+        // )
+
+        // /*
+        //  * INTERACTION #4
+        //  *
+        //  * User 111 responds to the bot with another `12.01`:
+        //  */
+        // event = require('../test/events/user_1_spesa_4')
+        // updateMemory(expectedMemory)
+        // expectedMemory = { ...expectedMemory, history: { user_1: 2402 } }
+
+        // await index.handler(event)
+
+        // expect(bucketMock.readJsonContent).toHaveBeenCalledTimes(4)
+        // expect(bucketMock.writeJsonContent).toHaveBeenCalledTimes(3)
+        // expect(bucketMock.writeJsonContent).toHaveBeenCalledWith(MEMORY_KEY, expectedMemory)
+        // expect(axiosMock.post).toHaveBeenCalledTimes(4)
+        // expect(axiosMock.post).toHaveBeenCalledWith(
+        //   SEND_MESSAGE_URL,
+        //   {
+        //     chat_id: 1,
+        //     text: 'user_1: 24.02'
+        //   }
+        // )
+      })
     })
   })
 })
---
+++ test/events/user_1_bad_input.js
@@ -0,0 +1,3 @@
+module.exports = {
+  body: '{"update_id":656537654,"message":{"message_id":64,"from":{"id":111,"is_bot":false,"first_name":"user_1","last_name":"subject","language_code":"en"},"chat":{"id":1,"first_name":"user_1","last_name":"subject","type":"private"},"date":9999999999,"reply_to_message":{"message_id":63,"date":9999999999,"text":"Quanto hai speso, user_1?"},"text":"hello"}}'
+}
